a8a3982639ba85b2a05cdd89e69220393a8ec0db
JoaoAlmeidaCAA <joao.almeida@comprealugueagora.com.br>
2024-05-09 17:50:16 -0300

    Project Euler

---
+++ Exercício 2/index.js
@@ -0,0 +1,26 @@
+//Calcular a soma dos números pares 
+function fibonacci(limit) {
+    let sum = 0;
+    let prev =  1;
+    let curr = 1;
+
+    //Looping até atingir o limite 
+    while(curr <= limit) {
+        // Verifica se o número é par e o adiciona a soma
+        if (curr % 2 === 0) {
+            sum += curr;
+        }
+    //Faz o calculo do próximo numero da sequência e os atualiza
+    let next = prev + curr;
+    prev = curr;
+    curr = next;
+    }
+    return sum;
+}
+
+// Define o limite
+const limit = 4000000;
+const result = fibonacci(limit);
+
+//Exibir resultado no console
+console.log ("A soma de todos os números pares de Fibonacci até" + limit + "é" + result);
