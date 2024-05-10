e16ea02b251b2335eaa2d99eb3012d9e10c4b103
JoaoAlmeidaCAA <joao.almeida@comprealugueagora.com.br>
2024-05-10 17:19:02 -0300

    Project Euler

---
+++ Exercício 3/index.js
@@ -0,0 +1,20 @@
+//Função para encontrar o maior fator primo de um número
+function maiorNprimo(number) {
+    let divisor = 2;
+    while (number > 1) {
+        if (number % divisor ===0) {
+            number /= divisor;
+        }
+        else{
+            divisor++;
+        }
+    }
+    return divisor;
+}
+
+//Encontrar o maior fator primo do número 
+const number = 600851475143;
+const maiorFator = maiorNprimo(number);
+
+//Mostra o resultado no console
+console.log("O resultado é", maiorFator);
