98b822e0c6de380128de96c403894524969a874f
JoaoAlmeidaCAA <joao.almeida@comprealugueagora.com.br>
2024-05-13 15:53:06 -0300

    Project Euler

--- Exercício 5/index.js
+++ Exercício 5/index.js
@@ -1,7 +1,7 @@
 //Função para verificar, se o número é divisivel por todos de 1 até 20
 function divisivelPorTodos(numero) {
     
-    for (let x = 1; x <= 20; i++) {
+    for (let x = 1; x <= 20; x++) {
         if (numero % x !== 0) {
             return false;
         }
@@ -9,3 +9,10 @@ function divisivelPorTodos(numero) {
     return true;
 }
 
+let numero = 1;
+
+while (!divisivelPorTodos(numero)) {
+    numero++;
+}
+
+console.log("O menor número divisível por todos entre 1 e 20 é", + numero);
