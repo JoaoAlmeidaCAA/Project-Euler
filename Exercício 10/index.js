17f5e964db1f80b18471ddbd65aee2dc5a2c8b72
JoaoAlmeidaCAA <joao.almeida@comprealugueagora.com.br>
2024-05-20 05:27:23 +0000

    Project Euler

---
+++ Exercício 10/index.js
@@ -0,0 +1,25 @@
+//Função para iniciar o codico somando 
+function somaPrimos (limite) {
+    let ePrimo = new Array(limite + 1).fill(true);
+    ePrimo[0] = ePrimo[1] = false;
+    
+    //Define todos os primos até 2000000
+    for (let x = 2; x * x <= limite; x++) {
+        if (ePrimo[x]) {
+            for (let y = x * x; y <= limite; y += x) {
+                ePrimo[y] = false;
+            }
+        }
+    }
+
+    //Soma todos os primos
+    let soma = 0;
+    for (let x = 2; x <= limite; x++) {
+        if (ePrimo[x]) {
+            soma += x;
+        }
+    }
+    return soma;
+}
+//Mostra no console o resultado
+console.log(somaPrimos(2000000));
